ef6e442a72d97f5bc5e52726edd41a70b40b4646
Adron Hall <adronhall@gmail.com>
2013-12-30 15:53:21 -0800

    Adding comment about the link where the JSON format will be described at https://github.com/Adron/symphonize/wiki/Json-format-for-configuration-based-data-generation.

--- bin/symphonize.js
+++ bin/symphonize.js
@@ -1,4 +1,17 @@
 // Load Chance
 var Chance = require('chance'),
-    chance = new Chance();
-symphonize = exports;
+    chance = new Chance(),
+    symphonize = exports;
+
+symphonize.generate_sample = function () {
+
+}
+
+symphonize.generate = function (gen_spec) {
+    // This function generates data based on the received specification.
+    // To learn more about the specification check out this link:
+    //   https://github.com/Adron/symphonize/wiki/Json-format-for-configuration-based-data-generation.
+
+
+}
+

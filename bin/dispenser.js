b1c4110b782297821c95b606213e6774763548d6
Adron Hall <adronhall@gmail.com>
2014-01-06 19:04:46 -0800

    Small refactor to pull the for loop into the right position.

--- bin/dispenser.js
+++ bin/dispenser.js
@@ -15,28 +15,29 @@
  * }
  */
 
+var db = require('orchestrate');
+
 function Dispenser(write_to) {
     this._write_to = write_to;
 }
 
 function write_to_orchestrate(data_to_write) {
 
+
 }
 
 function write_to_console(data_to_write) {
-
-    for (var i = 0; i < data_to_write.count; i++) {
-        console.log(data_to_write[i]);
-    }
+    console.log(data_to_write[i]);
 }
 
 Dispenser.prototype.write_it = function (data_to_write) {
-    if (this._write_to === "console") {
-        write_to_console(data_to_write);
-    } else if (this._write_to === "orchestrate") {
-        write_to_orchestrate(data_to_write);
+    for (var i = 0; i < data_to_write.count; i++) {
+        if (this._write_to === "console") {
+            write_to_console(data_to_write);
+        } else if (this._write_to === "orchestrate") {
+            write_to_orchestrate(data_to_write);
+        }
     }
-
     return 'success';
 }
 

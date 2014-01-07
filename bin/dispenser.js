69aef68c774c707b53b1f350183b69523a77e7bd
Adron Hall <adronhall@gmail.com>
2014-01-06 18:39:03 -0800

    Adding instruction for getting the orchestrate.js code working to push data to.

--- bin/dispenser.js
+++ bin/dispenser.js
@@ -4,7 +4,15 @@
  *  to the designated write source.
  *
  * Note: for the write_to_orchestrate section to work you'll have to add a file
- *  called orchestrate_key.js for the calls against Orchestrate.io to work.
+ *  called orchestrate_key.js for the calls against Orchestrate.io to work. The
+ *  file needs to be placed in a directory called "key" and then add the appropriate
+ *  key for access.
+ *
+ * The code for the key should look like this:
+ *
+ * function gimme_orchestrate_key(){
+ *  return "012fyour-key3-goes-98f3-hereffe33f4e";
+ * }
  */
 
 function Dispenser(write_to) {

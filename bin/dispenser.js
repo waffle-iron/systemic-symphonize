4c3db8e177c6bc1d997996fb98aa4a072a8e56b6
Adron Hall <adronhall@gmail.com>
2014-01-06 18:33:02 -0800

    Getting there.

--- bin/dispenser.js
+++ bin/dispenser.js
@@ -2,6 +2,9 @@
  * Created by Adron on 1/6/14.
  * Description: Purpose of this code is for exporting, or "dispensing" the data
  *  to the designated write source.
+ *
+ * Note: for the write_to_orchestrate section to work you'll have to add a file
+ *  called orchestrate_key.js for the calls against Orchestrate.io to work.
  */
 
 function Dispenser(write_to) {

3235e7b45944fc4512f2edd5c36fc81318f8a988
Adron Hall <adronhall@gmail.com>
2014-01-06 16:50:43 -0800

    Removing multi-gen, it died.

--- bin/multi_gen.js
+++
@@ -1,9 +0,0 @@
-var multi_gen = exports;
-
-multi_gen.gen = function (count, ident) {
-    var data = new Array();
-    for (var i = 0; i < count; i++) {
-        data.push(ident.ident_generate());
-    }
-    return data;
-}

1fe0bb5d3c22b5fa0bd0c8244b6e23e1c324799a
Adron Hall <adronhall@gmail.com>
2014-02-02 13:23:13 -0800

    Added test for verifying that the key is in place and ready for use. This is however the key that each prospective developer would need to have a local copy of. It is NOT included in the github public repo.

--- tests/_orchestrate_key.js
+++ tests/_orchestrate_key.js
@@ -1,4 +1,19 @@
 /**
  * Created by Adron on 2/2/14.
- * Simple object constructor test and verification of object constructors.
+ *
+ * Description: Simple object constructor test and verification of object constructors.
  */
+
+var should = require('should');
+var Orchestrate_key = require('../key/orchestrate_key');
+
+describe('the orchestrate key', function () {
+    it('should exist when constructed.', function () {
+        var orchestrate_key = new Orchestrate_key();
+        orchestrate_key.should.exist;
+    })
+    it('should return the key value.', function () {
+        var orchestrate_key = new Orchestrate_key();
+        orchestrate_key.access_key.should.exist;
+    })
+})
